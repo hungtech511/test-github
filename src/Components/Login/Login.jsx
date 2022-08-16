bd81a447e4dfa93ebf84dd218f9491a3786ad29c
tech2dev <viethung.tech2@gmail.com>
2022-08-16 21:22:06 +0700

    changed login.jsx and add css file

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import './Login.css'
+
 function Login(props) {
     return (
         <div>
             Login
+
+
+            Login changed second time
         </div>
     );
 }
